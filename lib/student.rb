88d8891b615d6809ace1572c0a429ce04781903d
Irina Kagan <irinagamb@gmail.com>
2019-05-13 00:41:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,13 @@
 class Student
 
-   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+
   @@all = []
 
   def initialize(student_hash)
-
+    student_hash.each {|key, value| self.send(("#{key}="), value)
+       @@all.push(self)
+    }
   end
 
   def self.create_from_collection(students_array)
