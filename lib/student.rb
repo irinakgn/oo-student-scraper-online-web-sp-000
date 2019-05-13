09def714334b6a28529861faa0e1fdda83b335dc
Irina Kagan <irinagamb@gmail.com>
2019-05-13 00:42:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,14 +11,14 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+     students_array.each {|student_hash| Student.new(student_hash) }
   end
 
   def add_student_attributes(attributes_hash)
-
+     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-
+ @@all
   end
 end
